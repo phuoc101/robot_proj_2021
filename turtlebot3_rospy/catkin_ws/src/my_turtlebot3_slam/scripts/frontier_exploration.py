822214d47c5e921503921eba06789afa941999ff
Phuoc Nguyen <phuoc.nguyen@tuni.fi>
2021-12-19 16:59:58 +0200

    Added comments

--- turtlebot3_rospy/catkin_ws/src/my_turtlebot3_slam/scripts/frontier_exploration.py
+++ turtlebot3_rospy/catkin_ws/src/my_turtlebot3_slam/scripts/frontier_exploration.py
@@ -32,6 +32,7 @@ prev_target = np.array([0, 0])
 rviz_id = 0
 bot_idx = np.zeros([1,2])
 
+# read information from the map
 def map_callback(data):
     global map_data
     global resolution, ori_x, ori_y, w, h
@@ -40,48 +41,50 @@ def map_callback(data):
     ori_x = map_data.info.origin.position.x
     ori_y = map_data.info.origin.position.y
 
+# callback of /odom subscriber
 def update_pose(data):
     global odom, bot_x, bot_y
     odom = data
     bot_x = odom.pose.pose.position.x
     bot_y = odom.pose.pose.position.y
 
+# update the position of the robot (the returned coordinate is only for visualization)
 def bot_index(x, y):
     return np.array([int((x - ori_x)/resolution), int((y - ori_y)/resolution)])
 
+# transform contour to real map coordinate
 def to_map_coord(contour):
     for index in range(len(contour)):
         contour[index][0] = round(contour[index][0] * resolution + ori_x, 2)
         contour[index][1] = round(contour[index][1] * resolution + ori_y, 2)
     return contour
 
-def get_bot_index_thread(name):
+# thread to constantly update the position of the turtlebot
+def get_bot_coord_thread(name):
     global bot_idx
     print(f"thread {name} starting")
     while True:
         time.sleep(1)
         bot_idx = np.array([bot_x, bot_y])
-        print(f'bot index {bot_idx}')
+        print(f'bot coordinate {bot_idx}')
 
+# calculate the frontiers and plot the position of the centroids, this function is only for visualization
 def get_frontier():
     fig, ax = plt.subplots()
     while map_data.header.seq<1 or len(map_data.data)<1:
         pass
     data = map_data.data
-    # print(bot_x, bot_y)
     w = map_data.info.width
     h = map_data.info.height
     data_np = np.asarray(data)
     data_np = np.reshape(data_np, [h, w]);
-    # bot_idx = bot_index(bot_x, bot_y)
     ## for visualizing ####
     plt.imshow(data_np, cmap='gray', vmin = -1, vmax = 100)
     plt.plot(bot_idx[0], bot_idx[1], 'bo--', linewidth=2, markersize=12)
     contour_m1 = find_contours(data_np, -1, fully_connected='high')
-    # edges
+    # detect groups of potential frontiers by finding the contour of occupied
+    # spots and unoccupied spots, then subtract them
     contour_0 = find_contours(data_np, 1, fully_connected='high')
-    # for contour in contour_0:
-    #     ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
     contour_m1 = np.concatenate(contour_m1, axis=0)
     contour_0 = np.concatenate(contour_0, axis=0)
     
@@ -91,19 +94,18 @@ def get_frontier():
     contour_0 = np.asarray(contour_0)
     contour_m1 = np.asarray(contour_m1)
 
-    # convert contour np arrays into sets
     set_0 = set([tuple(x) for x in contour_0])
     set_m1 = set([tuple(x) for x in contour_m1])
 
     candidates = list(set_m1.difference(set_0))
     candidates = np.asarray(candidates)
+
+    # group the frontiers using agglomerative (hierarchical clustering) 
     model = AgglomerativeClustering(n_clusters=3, affinity='euclidean', linkage='ward')
     model.fit(candidates)
     labels = model.labels_
-   
     centroids = np.zeros([3, 2])
     dis = np.zeros([3, 1])
-
     for i in range(3):
         centroids[i, :] = np.mean(candidates[labels==i, :], axis = 0)
         dis[i] = norm(centroids[i, :] - bot_idx)
@@ -119,8 +121,10 @@ def get_frontier():
 
     plt.show()
 
+# thread to find the frontiers and set target
 def get_frontier_thread(name, lock):
     print(f'Thread {name} is starting')
+    # these values can be changed
     n_cluster = 5
     knn = 7
     markerarray_publisher = rospy.Publisher('visualization_marker_array', MarkerArray, queue_size=10)
@@ -131,19 +135,16 @@ def get_frontier_thread(name, lock):
         while map_data.header.seq<1 or len(map_data.data)<1:
             pass
         data = map_data.data
-        # print(bot_x, bot_y)
         w = map_data.info.width
         h = map_data.info.height
         data_np = np.asarray(data)
         data_np = np.reshape(data_np, [h, w]);
         lock.acquire()
-        # bot_idx = bot_index(bot_x, bot_y)
-        # unknowns
+
+        # detect groups of potential frontiers by finding the contour of occupied
+        # spots and unoccupied spots, then subtract them
         contour_m1 = find_contours(data_np, -1, fully_connected='high')
-        # edges
         contour_0 = find_contours(data_np, 1, fully_connected='high')
-        # for contour in contour_0:
-        #     ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
         contour_m1 = np.concatenate(contour_m1, axis=0)
         contour_0 = np.concatenate(contour_0, axis=0)
         
@@ -157,50 +158,45 @@ def get_frontier_thread(name, lock):
         set_0 = set([tuple(x) for x in contour_0])
         set_m1 = set([tuple(x) for x in contour_m1])
 
-        # candidates = list(set_m1.difference(set_0))
         global target
 
+        # stop condition
         candidates = list(set_m1.difference(set_0))
         if len(candidates) < 20:
             print('done')
             break
-        # candidates = np.asarray(candidates)
+
+        # group the frontiers using agglomerative (hierarchical clustering) 
+        # and choose the nearest group with knn classifier
         model = AgglomerativeClustering(n_clusters=n_cluster, affinity='euclidean', linkage='ward')
         model.fit(candidates)
         labels = model.labels_
         lbl_ign = []
+        # ignore labels with too few candidates to avoid running into walls
         for i in range(n_cluster):
             if np.sum(labels==i) < 10:
                 lbl_ign.append(i)
                 print(f'ignore label {i}')
-    
         dis = np.ones([len(candidates), 1])*10000000
 
         for i in range(len(candidates)):
             if labels[i] not in lbl_ign:
                 dis[i] = norm(np.asarray(candidates[i]) - bot_idx)
         
-        
         candidates = np.asarray(candidates)
         
+        # implement knn classifier, choose the nearest cluster
         dis_sort = np.argsort(dis)
         nearest_idx = dis_sort[0:knn]
         potential_targets = labels[nearest_idx]
-        # print(f'potential targets: {potential_targets[:,0]}')
-        # print(potential_targets[:,0])
         target_label = np.argmax(np.bincount(potential_targets[:,0]))
         target = np.mean(candidates[labels==target_label, :], axis = 0)
 
-        # for i in range(n_cluster):
-        #     if np.sum(labels==i, axis=0) > 8:
-        #         centroids[i, :] = np.mean(candidates[labels==i, :], axis = 0)
-        #         dis[i] = norm(centroids[i, :] - bot_idx)
-        # print(f'min dis {dis}')
-        # target = centroids[np.argmin(dis)]
         print(f'next target {target}')
-        # print(f'chosen {target}')
+
         lock.release()
 
+        # visualize the candidates in RVIZ as a MarkerArray object
         dim = resolution*2
         markerArray = MarkerArray()
         # loop throgh all instances of the array
@@ -225,11 +221,9 @@ def get_frontier_thread(name, lock):
                 rviz_id = 0
         markerarray_publisher.publish(markerArray)
 
-
-
+# thread client of move_base, target updated constantly
 def movebase_thread(client, lock):
-    # # Create an action client "move_base"
-    # client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+    # Define Marker object to visualize the next target
     global rviz_id
     marker_publisher = rospy.Publisher('target', Marker, queue_size=10)
     marker_goal = Marker()
@@ -259,9 +253,9 @@ def movebase_thread(client, lock):
         goal.target_pose.pose.position.y = float(target[0]) 
         goal.target_pose.pose.orientation.w = 1.0
         print(f'new target {target}')
-        # No rotation of the mobile base frame w.r.t. map frame
         client.send_goal(goal)
 
+        # Visualize the next goal as a Marker object in RVIZ
         marker_goal.pose.orientation.w = 1.0
         marker_goal.pose.position.x = target[1]
         marker_goal.pose.position.y = target[0]
@@ -269,33 +263,29 @@ def movebase_thread(client, lock):
         marker_publisher.publish(marker_goal)
 
         lock.release()
-        client.wait_for_result()
 
-
+# end the program with ctrl C
 def signal_handler(sig, frame):
     print('Program ended by user')
     sys.exit()
 
-# If the python node is executed as main process (sourced directly)
 if __name__ == '__main__':
     #Initializes a rospy node to let the SimpleActionClient publish and subscribe
     rospy.init_node('movebase_client_py')
     rate = rospy.Rate(10)
-    # Create an action client "move_base"
+
     movebase_client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
     velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     odom_subscriber = rospy.Subscriber('/odom', Odometry, update_pose)
     map_client = rospy.Subscriber("/map", OccupancyGrid, map_callback)
-    vel_msg = Twist()
-    # x_goal = 0
-    # y_goal = 0
-    # result = movebase_client(x_goal, y_goal)
-    # if result:
-    #     rospy.loginfo("Goal execution done!")
+
     lock = threading.Lock()
 
+    ## uncomment the line below for visualization
     # get_frontier()
-    threadBotIdx = threading.Thread(target = get_bot_index_thread, args = ('bot index', ))
+
+    ## comment the part below to prevent robot from moving to visualize
+    threadBotIdx = threading.Thread(target = get_bot_coord_thread, args = ('bot index', ))
     threadBotIdx.daemon = True
     threadBotIdx.start()
 
